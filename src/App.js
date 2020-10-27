2f488822464da1dea146da2409cad7d7429f371f
Jack Boyle <jackmatthewboyel@gmail.com>
2020-10-27 21:43:59 +0000

    added main screen layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
-import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import { blue } from '@material-ui/core/colors';
+import ProgressDial from './components/progressDial';
 
-function App() {
+const useStyles = makeStyles(() => ({
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    backgroundColor: blue[900],
+    width: '100vw',
+    height: '100vh',
+    flexDirection: 'column',
+  },
+}));
+
+export default function App() {
+  const classes = useStyles();
+
   return (
-    <div className="App" />
+    <div className={classes.root}>
+      <h1>Per Diem</h1>
+      <ProgressDial />
+      <h5>Add Expense</h5>
+    </div>
   );
 }
-
-export default App;
---
+++ src/components/progressDial.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import CircularProgress from '@material-ui/core/CircularProgress';
+import { grey } from '@material-ui/core/colors';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    alignItems: 'center',
+  },
+  wrapper: {
+    margin: theme.spacing(1),
+    position: 'relative',
+  },
+  textWrapper: {
+    width: 200,
+    height: 200,
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'column',
+  },
+  dialText: {
+    margin: 0,
+    color: 'white',
+  },
+  fabProgress: {
+    color: grey[50],
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    zIndex: 1,
+  },
+}));
+
+export default function ProgressDial() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <div className={classes.wrapper}>
+        <div className={classes.textWrapper}>
+          <h2 className={classes.dialText}>&pound;3.25</h2>
+          <p className={classes.dialText}>Per Day</p>
+        </div>
+        <CircularProgress variant="static" value={80} size={200} className={classes.fabProgress} />
+      </div>
+    </div>
+  );
+}
